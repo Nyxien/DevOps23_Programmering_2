9fd057f45e565c69f256e020464e0702bd04c9a6
Edvin Jäderlund <edvin.jaderlund@gmail.com>
2023-10-19 09:05:43 +0200

    Remove venv files for performance

--- lektion_3/application/app.py
+++ lektion_3/application/app.py
@@ -1,7 +1,18 @@
 from flask import render_template, Flask
+import pandas as pd
+
 
 app = Flask(__name__)
+dictionary = {
+    "landsdel" : ["Götaland", "Götaland", "Götaland", "Svealand", "Svealand", "Norrland", "Norrland", "Norrland", "Norrland", "Norrland"],
+    "landskap" : ["Östergötland", "Östergötland", "Västergätland", "Södermanland", "Södermanland", "Norrbotten", "Gästrikland", "Ångermanland", "Ångermanland", "Ångermanland"],
+    "Stad" : ["Linköping", "Motala", "Mjölby", "Mariefred", "Nyköping", "Piteå", "Sandviken", "Sollefteå", "Kramfors", "Örnsköldsvik"]
+}
+df = pd.DataFrame(dictionary)
+html = df.to_html(classes="table")
+
 
 @app.route("/")
 def index():
-    return render_template("template.htm")
+    return render_template("template.htm", data=html)
+
